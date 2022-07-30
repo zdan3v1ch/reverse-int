191796b1c39de79393050840e08f38009db05225
Anatoli_Z <anatoli.zdanevitch@gmail.com>
2022-07-30 20:44:08 +0300

    feat: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let module = Math.abs(n).toString().split("");
+    
+    if (module[module.length - 1] == 0) {
+        module.pop();
+    }
+  let rev = module.reverse();
+  return rev.join("")
 }
